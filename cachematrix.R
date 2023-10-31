3a41c8e026d84bd2adbd6150798a7b3284dc1836
sas-r-python-programming <126130160+sas-r-python-programming@users.noreply.github.com>
2023-10-31 10:44:41 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Create a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inv) inverse <<- inv
+        getInverse <- function() inverse
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        if (!is.null(inverse)) {
+                message("Getting cached matrix")
+                return(inverse)
+        }
+        mat <- x$get()
+        inverse <- solve(mat, ...)
+        x$setInverse(inverse)
+        inverse
 }
+ 
+
+## test
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+
+print(my_matrix$get())
+
+print(my_matrix$getInverse())
+
+print(cacheSolve(my_matrix))
+
+print(my_matrix$getInverse())
